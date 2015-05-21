7d1f1bd7880361689a30c054ce6f49b48f1dd6b1
Tong <karankokatu@gmail.com>
2015-05-21 14:04:21 +0700

    addContent in js working

--- src/qt_ui/res/js/browser.js
+++ src/qt_ui/res/js/browser.js
@@ -1,6 +1,4 @@
-
 var CBrowser = new function () {
-
     var CBrowser = this;
     this.showFullId = function (div, fullId) {
         div.find("a.text").html(fullId.substring(0, 25) + "...");
@@ -18,10 +16,7 @@ var CBrowser = new function () {
             this.showFullId(div, fullId);
     };
     this.getImageFrJson = function (json) {
-//            console.log(json);
-//            console.log(json.content[0]);
         try {
-//            console.log(json.content[0]);
             var ctt = json.content[0];
             var k;
             for (k in ctt.content) {
@@ -36,8 +31,11 @@ var CBrowser = new function () {
     this.getImage = function (clink) {
         var cj = (BrowserAPI.getContentByLink(clink));
         r = this.getImageFrJson(cj);
-        return '<img src="data:image/jpg;base64,' + r + '" class="brimg"/>';
+        return this.createImgHtml(clink, r);
 
+    };
+    this.createImgHtml = function (clink, imgB64Data) {
+        return '<a ><img id="' + clink + '" src="data:image/jpg;base64,' + imgB64Data + '" class="brimg"/></a>';
     };
     this.toggleCmt = function (div) {
         var s = div.find("a.shrt").hasClass("short");
@@ -50,15 +48,38 @@ var CBrowser = new function () {
             div.find("a.shrt").addClass("short");
         }
     };
+    this.showFullImg = function (div) {
+        $("#fullImage").html(div.html());
+        $("#fullImage").find("img").removeClass("brimg");
+    };
+    this.shortenId = function (id) {
+        return id.substr(0, 10) + "...";
+    };
+    this.addContent = function (clink, ctt) {
+        var sdiv = $("#standard").clone(true, true);
+        sdiv.removeAttr("id");
+        sdiv.find(".id").find(".text").attr("fullid", ctt.poster[0]);
+        sdiv.find(".id").find(".text").html(this.shortenId(ctt.poster[0]));
+        sdiv.find(".ctt").html(this.createImgHtml(clink, this.getImageFrJson(ctt)));
+        $("#mainframe").prepend(sdiv.children());
+//        console.log(ctt);
+//        console.log(sdiv);
+    };
+    this.refreshNew = function () {
+        var clink = "ccc:11175.1";
+        var ctt = BrowserAPI.getContentByLink(clink);
+        this.addContent(clink, ctt);
+//        this.refreshEvent();
+    };
 
 };
 
 $(document).ready(function () {
-    $("#test").click(function () {
-//        alert("test!! " + BrowserAPI.createContent("tmp/tmp1.jpg"));
-        console.log(BrowserAPI.getContentByLink("ccc:4310.1"));
+    $("#refresh-btn").click(function () {
+        CBrowser.refreshNew();
     });
     $(".id").find("a.text").click(function () {
+        console.log('log');
         CBrowser.toggleFullId($(this).parent());
     });
     $(".id-follow-btn").click(function () {
@@ -70,7 +91,14 @@ $(document).ready(function () {
     $(".brctt").find("a.shrt").click(function () {
         CBrowser.toggleCmt($(this).parent());
     });
+    $(".ctt").click(function () {
+        CBrowser.showFullImg($(this));
+    });
 
+    $("#fullImage").click(function () {
+        $(this).html("");
+    });
     $("#test1").html(CBrowser.getImage("ccc:4310.1"));
-    $("#test2").html(CBrowser.getImage("ccc:4310.1"));
+    $("#test2").html(CBrowser.getImage("ccc:11175.1"));
+
 });
