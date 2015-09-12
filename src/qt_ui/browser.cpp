d01289a2f2f63e3ec8dcc6d99e42434bd7c98727
hd1 <hd1@ccc.com>
2015-09-12 22:55:02 +0800

    add lib path

--- src/qt_ui/browser.cpp
+++ src/qt_ui/browser.cpp
@@ -457,7 +457,7 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationName(QAPP_ORG_NAME);
     QApplication::setOrganizationDomain(QAPP_ORG_DOMAIN);
     QApplication::setApplicationName(QAPP_APP_NAME_DEFAULT);
-    
+    app->addLibraryPath(app->applicationDirPath()+"/plugins");
     GUIUtil::SubstituteFonts(GetLangTerritory());
     QFont new_font = app.font();
     new_font.setPointSize( 10 ); 
