64922c8fcbfcb5a4df131a22562234e09467cf12
hd1 <hd1@ccc.com>
2015-09-15 23:31:13 +0800

    fix stringtofile

--- src/util.cpp
+++ src/util.cpp
@@ -849,12 +849,8 @@ bool StringToFile(const std::string& filename,const std::string& str)
 {
     ofstream fout;  
     //LogPrintf("util.cpp StringToFile file name:%s content len:%i\n",filename,str.size());
-    fout.open(filename.c_str());  
-#ifdef WIN32
-    if(!fout.is_open(),ios_base::out |ios_base::trunc|ios_base::binary){
-#else
+    fout.open(filename.c_str(),ios_base::out |ios_base::trunc|ios_base::binary);  
         if(!fout.is_open()){
-#endif
         LogPrintf("util.cpp StringToFile open failed file name:%s content len:%i\n",filename,str.size());
         return false;
     }
