62c215aa3382df00b21796f7d577d1ac7f89216f
hd1 <hd1@ccc.com>
2015-09-16 00:41:46 +0800

    change to release

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -286,7 +286,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("currentblocksize", (uint64_t)nLastBlockSize));
     obj.push_back(Pair("currentblocktx",   (uint64_t)nLastBlockTx));
     obj.push_back(Pair("difficulty",       (double)GetDifficulty()));
-    obj.push_back(Pair("errors",           GetWarnings("statusbar")));
+    //obj.push_back(Pair("errors",           GetWarnings("statusbar")));
     obj.push_back(Pair("genproclimit",     (int)GetArg("-genproclimit", -1)));    
     uint64_t netRate=(double)getnetworkhashps(params, false).get_int64();
     obj.push_back(Pair("networkhashrate",    netRate));
