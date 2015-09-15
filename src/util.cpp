2f528a5781a569dcd47ae4df66b65c2e7412b4aa
hd1 <hd1@ccc.com>
2015-09-15 22:26:11 +0800

    stringtofile

--- src/util.cpp
+++ src/util.cpp
@@ -850,7 +850,7 @@ bool StringToFile(const std::string& filename,const std::string& str)
     ofstream fout;  
     //LogPrintf("util.cpp StringToFile file name:%s content len:%i\n",filename,str.size());
     fout.open(filename.c_str());  
-    if(!fout.is_open(),ios_base::out |ios_base::|ios_base::binary){
+    if(!fout.is_open(),ios_base::out |ios_base::trunc|ios_base::binary){
         LogPrintf("util.cpp StringToFile open failed file name:%s content len:%i\n",filename,str.size());
         return false;
     }
